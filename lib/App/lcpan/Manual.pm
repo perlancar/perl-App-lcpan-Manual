cfd9597172117f794d02f9b8b17ae9beb01877c5
perlancar (on pc-home) <perlancar@gmail.com>
2020-04-11 09:16:14 +0700

    Initial commit

---
+++ lib/App/lcpan/Manual.pm
@@ -0,0 +1,15 @@
+package App::lcpan::Manual;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Manual for App::lcpan
+
+=head1 DESCRIPTION
+
+This distribution contains the following documentation pages:
+
+# INSERT_MODULES_LIST -App::lcpan::Manual
---
+++ lib/App/lcpan/Manual/Cookbook.pm
@@ -0,0 +1,25 @@
+package App::lcpan::Manual::Cookbook;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: App::lcpan cookbook
+
+=head1 FOR MODULE USERS
+
+
+=head1 FOR MODULE AUTHORS
+
+=head2 Checking if your distribution depends on a non-core Perl module
+
+    % lcpan deps Your-Dist --exclude-core
+
+=head2 Finding out if there is any broken prerequisites
+
+To find out which prerequisites that your distributions specify but are
+currently not indexed (i.e. cannot be installed by users):
+
+    % lcpan author-deps YOURCPANID --broken
